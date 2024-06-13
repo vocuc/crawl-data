1243729ec7986874965fb306c50381fd7da5c30a
Vocuc <buithequantk40l@gmail.com>
2024-06-13 09:00:40 +0700

    no message

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -6,10 +6,10 @@ role=${CONTAINER_ROLE:-app}
 
 env=${APP_ENV:-production}
 
-# if [ "$env" != "local" ]; then
-#     echo "Caching configuration..."
-#     (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
-# fi
+if [ "$env" != "local" ]; then
+    echo "Caching configuration..."
+    (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+fi
 
 # if [ "$role" = "scheduler" ]; then
  
