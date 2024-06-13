03ab1a0938719ab2130c5c7c816ed3a53948af90
Vocuc <buithequantk40l@gmail.com>
2024-06-13 09:06:30 +0700

    no message

--- docker/app/start.sh
+++ docker/app/start.sh
@@ -11,14 +11,13 @@ if [ "$env" != "local" ]; then
     (cd /var/www/html && php artisan config:cache && php artisan route:cache && php artisan view:cache)
 fi
 
-# if [ "$role" = "scheduler" ]; then
+if [ "$role" = "scheduler" ]; then
  
-#     while [ true ]
-#         do
-#         php /var/www/html/artisan schedule:run --verbose --no-interaction &
-#         sleep 60
-#         done
-# else
-#     echo "Could not match the container role \"$role\""
-#     exit 1
-# fi
+    while [ true ]
+        do
+        php /var/www/html/artisan schedule:run & sleep 60
+        done
+else
+    echo "Could not match the container role \"$role\""
+    exit 1
+fi
